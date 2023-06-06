5480b5e424ee678d733042b8ba893c2b741d3d4e
HudsonAnselmo38 <hjunior42@outlook.com>
2023-06-06 19:39:38 -0300

    First steps in Java with spring boot

---
+++ 05_FirstStepsInJavaSpringBoot/rest-springboot-java-erudio/src/main/java/com/hudsonandrade/restspringbootjavaerudio/RestSpringbootJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package com.hudsonandrade.restspringbootjavaerudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringbootJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringbootJavaErudioApplication.class, args);
+	}
+
+}
---
+++ 05_FirstStepsInJavaSpringBoot/rest-springboot-java-erudio/src/test/java/com/hudsonandrade/restspringbootjavaerudio/RestSpringbootJavaErudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudsonandrade.restspringbootjavaerudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringbootJavaErudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
